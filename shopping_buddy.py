76dab0d367ba252ba6a51b31f7d17d16aab3ac6d
Esteban Campostrini <lesteban22@gmail.com>
2021-01-10 10:23:26 +0100

    Add arg parser. Rename main.py -> shopping_buddy.py

---
+++ shopping_buddy.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python3
+
+import yaml
+from collections import namedtuple, defaultdict
+import os
+import re
+import argparse
+
+from trello import TrelloClient
+from config import STORES_PATH, SHOPPING_LIST_PATH
+from store import Store
+from shopping_list import ShoppingList
+
+
+def main():
+  parser = argparse.ArgumentParser(
+      description="A minimalist grocery shopping organizer that creates Trello cards based " +
+      "on shopping lists")
+  parser.add_argument(
+      "--stores",
+      "-s",
+      dest="stores",
+      nargs="+",
+      help="Space separated paths to the files containing the stores",
+      required=True)
+  parser.add_argument(
+      "--lists",
+      "-l",
+      dest="shoppingLists",
+      nargs="+",
+      help="Space separated paths to the target shopping lists",
+      required=True)
+  args = parser.parse_args()
+
+  stores = {}
+  for storePath in args.stores:
+    stores.update(Store.fromFile(storePath))
+
+  # TODO support multiple shopping lists
+  shoppingList = ShoppingList.fromFile(args.shoppingLists[0])
+  # print("{}".format(shoppingList))
+  currenStore = stores[shoppingList.store]
+
+  for itemName, quantity in shoppingList:
+    currenStore.addItem(itemName, quantity)
+
+  print("Shopping list: ")
+  for item, qtty in currenStore:
+    print("{}: {}".format(item, qtty))
+
+  print("=======")
+
+  trelloClient = TrelloClient()
+  cardId = trelloClient.createCard(currenStore.name)
+  checklistId = trelloClient.createChecklist(cardId, shoppingList.name)
+  trelloClient.addItemsToChecklist(
+      checklistId,
+      ["{}: {}".format(k, v) for k, v in currenStore.getCart().items()],
+  )
+
+
+if __name__ == "__main__":
+  main()
